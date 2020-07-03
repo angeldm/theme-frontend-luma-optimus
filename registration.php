b671ad7de897152cce5549c66db5392b58610e1c
Àngel Díaz <angeldm@gmail.com>
2020-07-04 00:03:09 +0200

    first commit

---
+++ registration.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Copyright © Àngel Díaz, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Angeldm/luma-optimus', __DIR__);
