209aeb6862cfe399ab99181ec7a07648bec1f177
Apache <apache@fedora-mage.localdomain>
2020-09-16 20:40:47 +0200

    commit

--- README.md
+++
@@ -1,1 +0,0 @@
-# theme-frontend-luma-optimus
--- registration.php
+++ registration.php
@@ -1,9 +1,10 @@
 <?php
+
 /**
- * Copyright © Àngel Díaz, Inc. All rights reserved.
+ * Copyright © Magento, Inc. All rights reserved.
  * See COPYING.txt for license details.
  */
 
-use \Magento\Framework\Component\ComponentRegistrar;
+use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::THEME, 'frontend/Angeldm/luma-optimus', __DIR__);
